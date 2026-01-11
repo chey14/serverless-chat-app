8c16e89e8998e9d3bc0c92a6434a063ababc5312
chey14 <chaitanyareddy1405@gmail.com>
2026-01-12 01:06:34 +0530

    CI/CD test: add banner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -113,18 +113,12 @@ function App() {
   };
 
   return (
-  <div className="flex flex-col h-screen">
-    {/* CI/CD Test Banner */}
-    <div className="bg-green-600 text-white text-center py-2 font-bold">
-      🚀 CI/CD Pipeline Working Successfully
-    </div>
-
-    <div className="flex flex-1">
-      <Sidebar
-        me={nickname}
-        clients={clients}
-        setTarget={(target) => setNewTarget(target)}
-      />
+    <div className="flex">
+        <Sidebar
+          me={nickname}
+          clients={clients}
+          setTarget={(target) => setNewTarget(target)}
+        />
       <div className="flex-auto">
         <Conversation
           target={target}
@@ -133,8 +127,7 @@ function App() {
         />
       </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default App;
